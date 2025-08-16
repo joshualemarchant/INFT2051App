a3741f05ddba808ba9aa7cbfef84067516e5c386
Herns0 <151488915+Herns0@users.noreply.github.com>
2025-08-16 14:33:49 +1000

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace INFT2051App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace INFT2051App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
