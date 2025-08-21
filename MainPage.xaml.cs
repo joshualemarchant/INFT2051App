b24eafef6c5f64177e38aec5fd6c7cf8d6925c8d
Herns0 <151488915+Herns0@users.noreply.github.com>
2025-08-21 18:13:04 +1000

    Basic UI with nav bar

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,11 +1,40 @@
-﻿namespace INFT2051App;
+﻿using Microsoft.Maui.Controls;
+
+namespace INFT2051App;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		//NavigationPage.SetHasNavigationBar(this, false);
-		InitializeComponent();
-	}
-}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
+    private void OnQuestionClicked(object sender, EventArgs e)
+    {
+        if (sender is Button button)
+        {
+            // Get the parent StackLayout that contains both button and answer
+            if (button.Parent is StackLayout parentStack)
+            {
+                // Find the answer StackLayout (should be the second child)
+                var answerStack = parentStack.Children.OfType<StackLayout>().FirstOrDefault(x => x != button.Parent);
+
+                if (answerStack != null)
+                {
+                    // Toggle visibility
+                    answerStack.IsVisible = !answerStack.IsVisible;
+
+                    // Update button text to show expand/collapse state
+                    if (answerStack.IsVisible)
+                    {
+                        button.Text = button.Text.Replace("▶", "▼");
+                    }
+                    else
+                    {
+                        button.Text = button.Text.Replace("▼", "▶");
+                    }
+                }
+            }
+        }
+    }
+}
